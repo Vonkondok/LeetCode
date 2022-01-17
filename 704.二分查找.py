b6f0bc0d310d3fe6257855c3b42f80d460f161ef
Vonkondok <50097507+Vonkondok@users.noreply.github.com>
2022-01-17 11:48:43 +0800

    Create 704.二分查找.py

---
+++ 704.二分查找.py
@@ -0,0 +1,10 @@
+# 281013 20220111
+# python
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return nums.index(target) if target in nums else -1
